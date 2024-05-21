98f53f2d5bfc5b233a13ce1e6e040676e83e57ba
21120036 <thienan2003bt@gmail.com>
2024-05-22 00:32:48 +0700

    Init react app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import logo from './logo.svg'
+
+function App() {
+  return (
+    <div className="App">
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <div className='brief-info'>
+          <h1>SOFTWARE DESIGN</h1>
+          <h4>CQ2021/3 - Group 12</h4>
+        </div>
+      </header>
+
+    </div>
+  );
+}
+
+export default App;
