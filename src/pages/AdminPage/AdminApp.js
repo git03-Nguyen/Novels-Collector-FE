02c9640d451f7251a58001cd02ae3b70c9e5c03b
21120177 <minhhuy14012003@gmail.com>
2024-06-13 14:52:08 +0700

    remove redux from example admin template

--- src/pages/AdminPage/AdminApp.js
+++ src/pages/AdminPage/AdminApp.js
@@ -1,31 +1,12 @@
-import React, { Suspense, useEffect } from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import React, { Suspense } from 'react'
 import { ToastContainer } from 'react-toastify';
-import { CSpinner, useColorModes } from '@coreui/react'
+import { CSpinner } from '@coreui/react'
 import './scss/style.scss'
 // Containers
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 
 
 const AdminApp = () => {
-  const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme')
-  const storedTheme = useSelector((state) => state.theme)
-
-  useEffect(() => {
-    console.log('AdminApp useEffect')
-    const urlParams = new URLSearchParams(window.location.href.split('?')[1])
-    const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0]
-    if (theme) {
-      setColorMode(theme)
-    }
-
-    if (isColorModeSet()) {
-      return
-    }
-
-    setColorMode(storedTheme)
-  }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <>
--- src/pages/AdminPage/AppSidebar.js
+++ src/pages/AdminPage/AppSidebar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
 
 import {
   CCloseButton,
@@ -8,9 +7,7 @@ import {
   CSidebarBrand,
   CSidebarFooter,
   CSidebarHeader,
-  CSidebarToggler,
 } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
 
 import { AppSidebarNav } from './AppSidebarNav'
 
@@ -19,20 +16,12 @@ import logo from '../../assets/brand/logo.png'
 import navigation from './_nav'
 
 const AppSidebar = () => {
-  const dispatch = useDispatch()
-  const unfoldable = useSelector((state) => state.sidebarUnfoldable)
-  const sidebarShow = useSelector((state) => state.sidebarShow)
 
   return (
     <CSidebar
       className="border-end"
       colorScheme="dark"
       position="fixed"
-      unfoldable={unfoldable}
-      visible={sidebarShow}
-      onVisibleChange={(visible) => {
-        dispatch({ type: 'set', sidebarShow: visible })
-      }}
     >
       <CSidebarHeader className="align-self-center" >
         <CSidebarBrand to="/" >
@@ -41,14 +30,12 @@ const AppSidebar = () => {
         <CCloseButton
           className="d-lg-none"
           dark
-          onClick={() => dispatch({ type: 'set', sidebarShow: false })}
+
         />
       </CSidebarHeader>
       <AppSidebarNav items={navigation} />
       <CSidebarFooter className="border-top d-none d-lg-flex">
-        <CSidebarToggler
-          onClick={() => dispatch({ type: 'set', sidebarUnfoldable: !unfoldable })}
-        />
+
       </CSidebarFooter>
     </CSidebar>
   )
