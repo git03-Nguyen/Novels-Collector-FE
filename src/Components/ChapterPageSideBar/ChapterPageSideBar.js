c63386d03a9f5fcf808ae28360ad36538fb60cf8
21120036 <thienan2003bt@gmail.com>
2024-06-13 20:50:29 +0700

    refactor ChapterPageSidebar

--- src/Components/ChapterPageSideBar/ChapterPageSideBar.js
+++ src/Components/ChapterPageSideBar/ChapterPageSideBar.js
@@ -1,12 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import './ChapterPageSideBar.scss';
+import { toast } from "react-toastify";
 
 export default function ChapterPageSideBar({ novelName, novelPoster, novelAuthor, chapterList, sourceSlug, novelSlug, curChapterSlug, handleSiblingChapterClick }) {
 
+    const [curChapterID, setCurChapterID] = useState(4);
+    const [isDisplayFullList, setIsDisplayFullList] = useState(false);
+    const [displayChapterList, setDisplayChapterList] = useState(chapterList);
+
+    const getCurChapterIDFromList = () => {
+        const newCurChapterID = chapterList?.findIndex(chapter => chapter.slug === curChapterSlug);
+        if (newCurChapterID === -1) {
+            console.log("Error from ChapterPageSideBar: Cur chapter not found in chapter list !");
+            return 0;
+        } else if (newCurChapterID !== curChapterID) {
+            setCurChapterID(newCurChapterID);
+        }
+
+        return newCurChapterID;
+    }
+
     const setActiveChapter = () => {
-        chapterList?.forEach((chapter, index) => {
+        displayChapterList?.forEach((chapter, index) => {
             const curChapterButton = document.querySelector(`#recent-novel-offcanvas-card-${index}`)
             if (curChapterButton) {
                 if (chapter?.slug === curChapterSlug) {
@@ -18,19 +35,40 @@ export default function ChapterPageSideBar({ novelName, novelPoster, novelAuthor
         });
     }
 
-    const handleClickChapter = async (destChapterID) => {
-        const curChapterID = chapterList?.findIndex(chapter => chapter.slug === curChapterSlug);
-        if (curChapterID === -1) {
-            console.log("Error from ChapterPageSideBar: Cur chapter not found in chapter list !");
+    const handleClickChapter = async (destChapterSlug) => {
+        const newCurChapterID = getCurChapterIDFromList();
+        const destChapterID = chapterList.findIndex(chapter => chapter.slug === destChapterSlug);
+        if (destChapterID === -1) {
+            toast.error("Error changing chapter: Cannot find destined chapter");
             return;
         }
-
-        handleSiblingChapterClick(parseInt(destChapterID) - parseInt(curChapterID));
+        handleSiblingChapterClick(parseInt(destChapterID) - parseInt(newCurChapterID));
     }
 
 
     useEffect(() => {
         setActiveChapter();
+    }, [displayChapterList])
+
+
+    useEffect(() => {
+        const newChapterID = getCurChapterIDFromList();
+        console.log(`New chapter id: ${newChapterID}`);
+        console.log("Sliced chapter list: ");
+        console.log(chapterList.slice(newChapterID, newChapterID + 10));
+        // console.log(chapterList.slice(newChapterID - 4, newChapterID + 5));
+
+        const topListIndex = parseInt(newChapterID / 10) * 10;
+        const bottomListIndex = topListIndex + 10;
+        const newDisplayChapterList = isDisplayFullList === true ? chapterList : chapterList.slice(topListIndex, bottomListIndex);
+
+        console.log("New display chapter list: ");
+        console.log(newDisplayChapterList);
+        setDisplayChapterList(newDisplayChapterList)
+    }, [isDisplayFullList, curChapterID, chapterList])
+
+    useEffect(() => {
+        getCurChapterIDFromList();
     }, [curChapterSlug])
 
     return (
@@ -59,23 +97,31 @@ export default function ChapterPageSideBar({ novelName, novelPoster, novelAuthor
                     </div>
                 </div>
 
-                {chapterList && chapterList?.length > 0 && chapterList?.map((chapter, index) => {
-                    return <button key={`recent-novel-offcanva-card-${index}`} id={`recent-novel-offcanvas-card-${index}`}
-                        className="btn btn-secondary offcanvas-novel-card-mini"
-                        onClick={() => handleClickChapter(index)}>
-                        <span>
-                            {sourceSlug !== 'DTruyenCom' && <span>Chương {chapter?.number} - </span>}
+                {displayChapterList && displayChapterList?.length > 0 &&
+                    displayChapterList?.map((chapter, index) => {
+                        return <button key={`recent-novel-offcanva-card-${index}`} id={`recent-novel-offcanvas-card-${index}`}
+                            className="btn btn-secondary offcanvas-novel-card-mini"
+                            onClick={() => handleClickChapter(chapter.slug)}>
+                            <span>
+                                {sourceSlug !== 'DTruyenCom' && <span>Chương {chapter?.number} - </span>}
 
-                            {chapter.title?.length <= 40
-                                ? chapter.title
-                                : chapter.title.slice(0, 41) + ' ...'
-                            }
+                                {chapter.title?.length <= 40
+                                    ? chapter.title
+                                    : chapter.title.slice(0, 41) + ' ...'
+                                }
 
-                        </span>
+                            </span>
 
 
-                    </button>
-                })}
+                        </button>
+                    })}
+
+                <button className="btn btn-secondary my-3"
+                    onClick={() => setIsDisplayFullList(!isDisplayFullList)}
+                >
+                    {isDisplayFullList === true ? "Thu gọn" : "Xem thêm"}
+                </button>
+
 
             </div>
         </div>
